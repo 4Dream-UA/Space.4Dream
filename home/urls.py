da7e706c033f66fed438f1ea7a755d37f1f5f1e2
Евгений Наволоков <goustg62@gmail.com>
2025-07-08 18:21:27 +0300

    Add path 'invite/' by CreateInviteView route

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, UpdateSettingsView
+from .views import HomePageView, UpdateSettingsView, CreateInviteView
 
 app_name = "home"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
     path("settings/<int:pk>", UpdateSettingsView.as_view(), name="settings"),
+    path("invite", CreateInviteView.as_view(), name="invite"),
 ]
