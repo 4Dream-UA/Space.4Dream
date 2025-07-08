a1d3274670bbef5186b20e05e556d9a08f7d981e
Евгений Наволоков <goustg62@gmail.com>
2025-07-08 17:19:03 +0300

    Add path to account settings

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, UpdateSettingsView
 
 app_name = "home"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
+    path("settings/<int:pk>", UpdateSettingsView.as_view(), name="settings"),
 ]
