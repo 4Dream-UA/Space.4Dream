a7ea0ab5db10d3a74dff5532842a490c0da08e2c
Евгений Наволоков <goustg62@gmail.com>
2025-07-08 13:44:54 +0300

    Add task type value and position value to public_config.py file

---
+++ public_config.py
@@ -0,0 +1,51 @@
+TASK_TYPE_VALUE = [
+    "bug",
+    "feature",
+    "change",
+    "refactoring",
+    "review",
+    "quality assurance"
+]
+
+POSITION_VALUE = {
+    "Senior Python Developer":
+        {
+            "name": "Python Developer",
+            "rank:": "Senior",
+        },
+    "Middle Python Developer":
+        {
+            "name": "Python Developer",
+            "rank:": "Middle",
+        },
+    "JavaScript Developer":
+        {
+            "name": "JavaScript Developer",
+        },
+    "Senior Project Manager":
+        {
+            "name": "Project Manager",
+            "rank:": "Senior",
+        },
+    "Project Manager":
+        {
+            "name": "Project Manager",
+        },
+    "QA Engineer":
+        {
+            "name": "QA Engineer",
+        },
+    "Designer":
+        {
+            "name": "Designer",
+        },
+    "Senior DevOps":
+        {
+            "name": "DevOps",
+            "rank:": "Senior",
+        },
+    "DevOps":
+        {
+            "name": "DevOps",
+        },
+}
