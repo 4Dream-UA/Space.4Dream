17426010e417fa26a1edf16f527cb432a47ee177
Евгений Наволоков <goustg62@gmail.com>
2025-07-08 17:20:42 +0300

    Add UpdateSettingsView router for settings/<int:pk>

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import get_user, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 
 
 class HomePageView(View):
@@ -7,3 +11,17 @@ class HomePageView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class UpdateSettingsView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    fields = [
+        "username", "email",
+        "first_name", "last_name",
+              ]
+    template_name = "home/settings.html"
+    success_url = reverse_lazy("home:index")
+
+class UpdateUserPassword(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    fields = []
