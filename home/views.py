3ebdcec20962d28ac1910b0971ed7e60c09b8c57
Евгений Наволоков <goustg62@gmail.com>
2025-07-09 20:55:04 +0300

    Add test for able to invite user in CreateInviteView

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth import get_user, get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import UpdateView, CreateView, ListView
 from django.urls import reverse_lazy
 
+from config.public_config import invite_able_returning
 from .forms import RegisterInviteForm
 
 
@@ -15,11 +16,16 @@ class HomePageView(View):
         return render(request, self.template_name)
 
 
-class CreateInviteView(LoginRequiredMixin, CreateView):
+class CreateInviteView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = get_user_model()
     form_class = RegisterInviteForm
     template_name = "home/invite.html"
     success_url = reverse_lazy("home:index")
+
+    def test_func(self) -> bool:
+        if self.request.user.position.name in invite_able_returning():
+            return True
+        return False
 
 class UpdateSettingsView(LoginRequiredMixin, UpdateView):
     model = get_user_model()
