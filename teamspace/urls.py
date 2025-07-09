b2a821cb080e1f626b34718b6cf269d0ad7bde64
Евгений Наволоков <goustg62@gmail.com>
2025-07-09 14:38:10 +0300

    Add AllMembersView router, crate classes CreateProjectView, CreateTeamView for future

--- teamspace/urls.py
+++ teamspace/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
+
+from .views import AllMembersView
+
 
 app_name = "teamspace"
 
 urlpatterns = [
-
+    path("all_members", AllMembersView.as_view(), name="all_members"),
 ]
--- teamspace/views.py
+++ teamspace/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from .models import Project, Team
+
+
+class AllMembersView(ListView):
+    model = Team
+    context_object_name = "all_members"
+    template_name = "teamspace/all_members.html"
+
+
+class CreateProjectView(CreateView):
+    model = Project
+    ...
+
+
+class CreateTeamView(CreateView):
+    model = Team
+    fields = '__all__'
+    template_name = ""
