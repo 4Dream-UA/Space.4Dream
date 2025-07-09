85960c0714c6bd63ac0526b02670bd55e77414f9
Евгений Наволоков <goustg62@gmail.com>
2025-07-09 15:06:19 +0300

    Add path to teamspace:create_team by CreateTeamView router

--- teamspace/urls.py
+++ teamspace/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import AllMembersView
-
+from .views import AllMembersView, CreateTeamView
 
 app_name = "teamspace"
 
 urlpatterns = [
     path("all_members", AllMembersView.as_view(), name="all_members"),
+    path("create_team", CreateTeamView.as_view(), name="create_team"),
 ]
