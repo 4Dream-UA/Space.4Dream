d18e914bbef5c36f1e6eacfdc51a8391f27c5f6c
Евгений Наволоков <goustg62@gmail.com>
2025-07-09 16:57:52 +0300

    Add path to user profile

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from .views import HomePageView, UpdateSettingsView, CreateInviteView
+from .views import HomePageView, UpdateSettingsView, CreateInviteView, ProfileView
 
 app_name = "home"
 
@@ -7,4 +9,8 @@ urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
     path("settings/<int:pk>", UpdateSettingsView.as_view(), name="settings"),
     path("invite", CreateInviteView.as_view(), name="invite"),
+    path("profile", ProfileView.as_view(), name="profile"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
