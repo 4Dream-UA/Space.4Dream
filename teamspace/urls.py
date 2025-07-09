33f94eafa048d7ad9dd0c77f05dce7d2ee7138b8
Евгений Наволоков <goustg62@gmail.com>
2025-07-09 20:58:36 +0300

    Add path to team_list by ListTeamView router

--- teamspace/urls.py
+++ teamspace/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import AllMembersView, CreateTeamView
+
+from .views import AllMembersView, CreateTeamView, ListTeamView
 
 app_name = "teamspace"
 
 urlpatterns = [
     path("all_members", AllMembersView.as_view(), name="all_members"),
     path("create_team", CreateTeamView.as_view(), name="create_team"),
+    path("teams", ListTeamView.as_view(), name="teams_list"),
 ]
