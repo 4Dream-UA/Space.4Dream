ebe38d1056a5da9ee68dba9264c6d6b08d1f719e
Евгений Наволоков <goustg62@gmail.com>
2025-07-10 11:12:07 +0300

    Add avatar field to fields test

--- home/tests/tests_forms/fields_tests.py
+++ home/tests/tests_forms/fields_tests.py
@@ -5,5 +5,9 @@ from home.forms import RegisterInviteForm
 class RegisterInviteFormTests(TestCase):
     def test_form_has_expected_fields(self):
         form = RegisterInviteForm()
-        expected_fields = ["username", "password1", "password2", "first_name", "last_name", "email", "position"]
+        expected_fields = [
+            "username", "password1", "password2",
+            "first_name","last_name", "email",
+            "position", "avatar",
+        ]
         self.assertEqual(sorted(list(form.fields.keys())), sorted(expected_fields))
