f78a157666870015fec5732286a879c5c8b5663a
Евгений Наволоков <goustg62@gmail.com>
2025-07-10 15:02:43 +0300

    Add path by DeleteMembersView and EditMembersView as teamspace:edit_member and teamspace:delete_member

--- teamspace/urls.py
+++ teamspace/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 
-from .views import AllMembersView, CreateTeamView, ListTeamView
+from .views import AllMembersView, CreateTeamView, ListTeamView, EditMembersView, DeleteMembersView
 
 app_name = "teamspace"
 
 urlpatterns = [
     path("all_members", AllMembersView.as_view(), name="all_members"),
+    path("edit_menu/<int:pk>", EditMembersView.as_view(), name="edit_member"),
+    path("delete_menu/<int:pk>", DeleteMembersView.as_view(), name="delete_member"),
     path("create_team", CreateTeamView.as_view(), name="create_team"),
     path("teams", ListTeamView.as_view(), name="teams_list"),
 ]
