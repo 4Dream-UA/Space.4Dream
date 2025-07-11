ea55f7eb91d71b0b6f26a3d64acb9e26839997a8
Евгений Наволоков <goustg62@gmail.com>
2025-07-11 14:38:08 +0300

    Add path to update status by UpdateTaskStatusView router

--- teamspace/urls.py
+++ teamspace/urls.py
@@ -6,7 +6,7 @@ from .views import (
     EditMembersView, DeleteMembersView, EditTeamView, DeleteTeamView,
     CreateProjectView, MemberProjectView, ListProjectView,
     UpdateProjectView, DeleteProjectView, TaskProjectView,
-    CreateTaskView, CompleteTaskView
+    CreateTaskView, CompleteTaskView, UpdateTaskStatusView
 )
 
 app_name = "teamspace"
@@ -15,16 +15,21 @@ urlpatterns = [
     path("all_members/", AllMembersView.as_view(), name="all_members"),
     path("edit_menu/<int:pk>/", EditMembersView.as_view(), name="edit_member"),
     path("delete_menu/<int:pk>/", DeleteMembersView.as_view(), name="delete_member"),
+
     path("teams/", ListTeamView.as_view(), name="teams_list"),
     path("create_team/", CreateTeamView.as_view(), name="create_team"),
     path("update_team/<int:pk>", EditTeamView.as_view(), name="edit_team"),
     path("delete_team/<int:pk>", DeleteTeamView.as_view(), name="delete_team"),
+
     path("projects/", ListProjectView.as_view(), name="project_list"),
     path("create_project/", CreateProjectView.as_view(), name="create_project"),
     path("update_project/<int:pk>", UpdateProjectView.as_view(), name="update_project"),
     path("delete_project/<int:pk>", DeleteProjectView.as_view(), name="delete_project"),
+
     path("member_project/<int:pk>/", MemberProjectView.as_view(), name="member_project"),
+
     path("task_project/<int:pk>/", TaskProjectView.as_view(), name="task_project"),
     path("create_task/<int:pk>", CreateTaskView.as_view(), name="create_task"),
     path("complete_task/<int:pk>", CompleteTaskView.as_view(), name="complete_task"),
+    path("status_task/<int:pk>/", UpdateTaskStatusView.as_view(), name="status_task"),
 ]
