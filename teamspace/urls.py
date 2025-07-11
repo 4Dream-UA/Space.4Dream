80c8d48e3811a4a4e32f921fdb87bc26b4ff7e76
Евгений Наволоков <goustg62@gmail.com>
2025-07-11 10:59:21 +0300

    Add path to CRUD operation router for ProjectView

--- teamspace/urls.py
+++ teamspace/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from .views import (
     AllMembersView, CreateTeamView, ListTeamView,
     EditMembersView, DeleteMembersView, EditTeamView, DeleteTeamView,
-    CreateProjectView, MemberProjectView
+    CreateProjectView, MemberProjectView, ListProjectView,
+    UpdateProjectView, DeleteProjectView
 )
 
 app_name = "teamspace"
@@ -17,6 +18,9 @@ urlpatterns = [
     path("create_team/", CreateTeamView.as_view(), name="create_team"),
     path("update_team/<int:pk>", EditTeamView.as_view(), name="edit_team"),
     path("delete_team/<int:pk>", DeleteTeamView.as_view(), name="delete_team"),
+    path("projects/", ListProjectView.as_view(), name="project_list"),
     path("create_project/", CreateProjectView.as_view(), name="create_project"),
-    path("memeber_project/<int:pk>/", MemberProjectView.as_view(), name="member_project"),
+    path("update_project/<int:pk>", UpdateProjectView.as_view(), name="update_project"),
+    path("delete_project/<int:pk>", DeleteProjectView.as_view(), name="delete_project"),
+    path("member_project/<int:pk>/", MemberProjectView.as_view(), name="member_project"),
 ]
